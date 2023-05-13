d4a86e9424bc149fccbc5f5256aff7456afdafc1
Joshua Trono <josh.trono@gmail.com>
2023-05-13 01:40:18 -0400

    added github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
 import Homepage from './components/Homepage';
 import Plan from './components/Plan';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/5year" element={<Plan />} />
-        </Routes>
-    </BrowserRouter>
+    <HashRouter>
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/5year" element={<Plan />} />
+    </Routes>
+  </HashRouter>
   );
 }
 
